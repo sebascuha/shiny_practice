22ad9600355f8f676b07347bce494977ffc65d93
Sebastian Cuellar <sebastian_cuellar@outlook.com>
2025-10-29 23:36:37 -0500

    Add interactive histogram and a card with a DF sample

--- app.py
+++ app.py
@@ -7,34 +7,50 @@ from shiny import reactive
 from shiny.express import input, ui, render
 from shinywidgets import render_plotly
 
+# Maing page parameters
+ui.page_opts(title = "Demo", fillable = True)
 # UI mode
 # ui.input_dark_mode() 
 
-# Real csv data
+# Real csv data loading
 @reactive.calc
 def dat():
-    infile = Path(__file__).parent / "shiny-python-projects/sales/data/sales.csv"
+    infile = Path(__file__).parent / "sales/data/sales.csv"
     return pd.read_csv(infile)
 
+# Display DF in a card
+with ui.card():
+    # Card title
+    ui.card_header("Sample Sales Data")
+    # Rendering it as DF
+    @render.data_frame
+    def sample_data():
+        return dat().head(30)
+
+# Input asking
 ui.input_numeric("n", "Numeric input", 5, min = 2, max = 10) 
 
-ui.page_opts(title = "Demo", fillable=True)
-with ui.layout_columns():
+@render_plotly
+def top_products():
+    df = dat().groupby("product")['quantity_ordered'].sum().\
+        nlargest(input.n()).reset_index()
+    return px.bar(df, x = "product", y = "quantity_ordered", 
+                  title = f"Top {input.n()} largest ordered products")
 
-    # @render.data_frame
-    # def data():
-    #     return dat()
-
-    @render_plotly
-    def plot1():
-        df = dat().groupby("product")['quantity_ordered'].sum().\
-            nlargest(input.n()).reset_index()
-        return px.bar(df, x = "product", y = "quantity_ordered",
-                      title = f"Top {input.n()} largest ordered products")
-
-    # @render_plotly
-    # def plot2():
-    #     return px.histogram(px.data.tips(), y="total_bill")
+# Input asking
+ui.input_selectize( "selectize",
+                   "Select an option below:",
+                   {"quantity_ordered": "Quantity Ordered",
+                    "price_each": "Price Each"} 
+                    )  
+# @render.text
+# def value():
+#     return f"{input.selectize()}"
+@render_plotly
+def plot2():
+    df = dat()
+    return px.histogram(df, x = input.selectize(), nbins = 10,
+                        title = f"Histogram of {input.selectize()}" )
     
 
  
